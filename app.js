6df38e9da5dbcc56fba876a2172f552b9aa66684
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-02-24 10:26:16 -0300

    Adicionando variaveis da DOM no script js

---
+++ app.js
@@ -0,0 +1,5 @@
+const TOTAL = document.getElementById("quantidade").value;
+const INITIAL = document.getElementById("de").value;
+const FINAL = document.getElementById("ate").value;
+const RESULT = document.getElementById("text");
+

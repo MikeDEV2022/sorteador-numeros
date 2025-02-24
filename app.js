344a95f54f97a90ed40b89cfdc887759be8d989e
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-02-24 14:53:08 -0300

    logica de geração dos números finaizada

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const TOTAL = document.getElementById("quantidade").value;
-const INITIAL = document.getElementById("de").value;
-const FINAL = document.getElementById("ate").value;
-const RESULT = document.getElementById("text");
-
+function sortear(){
+    let total = document.getElementById("quantidade").value;
+    let initial = parseInt(document.getElementById("de").value);
+    let final = parseInt(document.getElementById("ate").value);
+    let resultado = document.getElementById("text");
+    let aleatorio = [];
+    for(let i = 1; i <= total; i++){
+        aleatorio.push(Math.floor(Math.random() * (final - initial + 1)) + initial)
+    }
+    resultado.innerHTML = `Números sorteados:  ${aleatorio}`
+}

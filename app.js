721d0d888792982614c76a7c072f2873baf1012a
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-02-25 16:10:28 -0300

    Algoritmo do exercicio 100% finalizado

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ function sortear(){
     let final = parseInt(document.getElementById("ate").value);
     let resultado = document.getElementById("text");
     let aleatorio = [];
-    for(let i = 1; i <= total; i++){
-        aleatorio.push(Math.floor(Math.random() * (final - initial + 1)) + initial)
+    if(initial > final){
+        alert("Verifique os valores informados!");
+    }else{
+        for(let i = 1; i <= total; i++){
+            aleatorio.push(Math.floor(Math.random() * (final - initial + 1)) + initial)
+        }
+        resultado.innerHTML = `Números sorteados:  ${aleatorio}`
+        chamaResultado();
     }
-    resultado.innerHTML = `Números sorteados:  ${aleatorio}`
-    chamaResultado();
+    
 }
 function chamaResultado(){
     let ativador = document.querySelector(".container__botao-desabilitado");
@@ -17,12 +22,16 @@ function chamaResultado(){
 }
 function reiniciar(){
     let ativador = document.getElementById("btn-reiniciar");
-    ativador.classList.remove("container__botao");
-    ativador.classList.add("container__botao-desabilitado");
-    document.getElementById("quantidade").value = '';
-    document.getElementById("de").value = '';
-    document.getElementById("ate").value = '';
-    retornaBase();
+    if(ativador.classList == "container__botao"){
+        ativador.classList.remove("container__botao");
+        ativador.classList.add("container__botao-desabilitado");
+        document.getElementById("quantidade").value = '';
+        document.getElementById("de").value = '';
+        document.getElementById("ate").value = '';
+        retornaBase();
+    }else if(ativador.classList == "container__botao-desabilitado"){
+        alert("Faça o primeiro sorteio!")
+    }
 }
 function retornaBase(){
     document.getElementById("text").innerText = "Números sorteados:  nenhum até agora"

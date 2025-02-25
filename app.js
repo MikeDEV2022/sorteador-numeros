c716e3621314433fb85c4e6cf248551a04a26dae
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-02-25 14:54:32 -0300

    Adicionadas as logicas de mudança e implementação do botão de reiniciar

--- app.js
+++ app.js
@@ -8,4 +8,22 @@ function sortear(){
         aleatorio.push(Math.floor(Math.random() * (final - initial + 1)) + initial)
     }
     resultado.innerHTML = `Números sorteados:  ${aleatorio}`
+    chamaResultado();
+}
+function chamaResultado(){
+    let ativador = document.querySelector(".container__botao-desabilitado");
+    ativador.classList.remove("container__botao-desabilitado")
+    ativador.classList.add("container__botao")
+}
+function reiniciar(){
+    let ativador = document.getElementById("btn-reiniciar");
+    ativador.classList.remove("container__botao");
+    ativador.classList.add("container__botao-desabilitado");
+    document.getElementById("quantidade").value = '';
+    document.getElementById("de").value = '';
+    document.getElementById("ate").value = '';
+    retornaBase();
+}
+function retornaBase(){
+    document.getElementById("text").innerText = "Números sorteados:  nenhum até agora"
 }
